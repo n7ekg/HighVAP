2c9102fd192371ba2f3155d2011056d666bde3be
Ed Carp <erc@pobox.com>
2020-11-03 09:44:17 -0600

    Bug fix

--- HighVAP.cpp
+++ HighVAP.cpp
@@ -762,10 +762,6 @@ SCSFExport scsf_HighVAP(SCStudyInterfaceRef sc)
    LOBCurBidVolume[sc.Index] = xLOBCurBidVolume;
    LOBCurAskVolume[sc.Index] = xLOBCurAskVolume;
    CurPrice[sc.Index] = sc.Close[sc.Index];
-	VWAPPrice.DrawStyle = DRAWSTYLE_LINE;
-	VWAPPrice.LineWidth = 2;
-	VWAPPrice.PrimaryColor = RGB(255,0,255); // purple/magenta
-	VWAPPrice.LineLabel = LL_DISPLAY_VALUE | LL_VALUE_ALIGN_CENTER | LL_VALUE_ALIGN_RIGHT;   
    /*
    if (MaxBidVolume > MaxAskVolume)
 	   MaxVAP.PrimaryColor[sc.Index] = COLOR_RED;
